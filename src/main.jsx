e574d54cb7ad4d88710dfff6076f6164c5c10ab2
victormmorales <victor.morales@criptan.com>
2022-07-20 14:46:05 +0200

    store implementation

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
